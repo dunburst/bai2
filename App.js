00680a0d3bf6f7a6cbda1e8c46ab061d01b19452
dunburst <doanthanhhung2004@gmail.com>
2024-08-15 22:10:31 +0700

    bai2

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Button, Alert, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const showAlert = () => {
+    Alert.alert("hello");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Press Me" onPress={showAlert} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default App;
